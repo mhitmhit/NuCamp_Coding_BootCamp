d57c0e77425f6faf1828b19b57b3c9a5d8a64c4d
YE <yassine.elkabaili@gmail.com>
2024-04-26 13:02:05 -0400

    python 4-25-24

--- Python/1-Fundamentals/cc_while.py
+++ Python/1-Fundamentals/cc_while.py
@@ -1,15 +1,16 @@
 
 x = 0
 
-while x != 10:
-    x = x + 0.1
-    if x > 1:
-        break
-    if x < 5:
+while x < 11:
+    if x < 5 and x != 0:
         print(x)
-    elif x == 6:
-        break
-    elif x >= 5 and x <= 8:
+    elif x == 5:
         print("x is bigger then or equal to 5, and less then or equal to 8, but not 6. It is:", x)
-    else:
+    elif x >= 5 and x <= 8:
+        if x == 6:
+            print(x)
+        else:
+            print("x is bigger then or equal to 5, and less then or equal to 8, but not 6. It is:", x)
+    if x > 8:
         print("x is bigger than 8. It is:", x)
+    x = x + 1
