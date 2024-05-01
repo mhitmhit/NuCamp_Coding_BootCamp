12a9de30d311b5b80a0d537d763cdb446ec5fe6e
ye desktop <ye32@njit.edu>
2024-05-01 06:51:29 -0400

    4-31-24

---
+++ Python/1-Fundamentals/built_in_functions.py
@@ -0,0 +1,17 @@
+# built in functions
+print("math functions")
+
+value = abs(-1)
+print(value)
+
+value2 = float(400)
+print(value2)
+
+value3 = int(1.233)
+print(value3)
+
+value4 = max(1,2,3,4,10)
+print(value4)
+
+value5 = pow(2,3)
+print(value5)
