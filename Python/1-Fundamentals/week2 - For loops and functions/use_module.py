d0f1f6a7497a5c91bde0cca7708d909fc833f636
ye desktop <ye32@njit.edu>
2024-05-03 23:09:51 -0400

    re arranged week 1 and 2 files

---
+++ Python/1-Fundamentals/week2 - For loops and functions/use_module.py
@@ -0,0 +1,11 @@
+# import my_module
+
+# my_module.greet("einstein")
+# print("my favorite ice cream flavor is", my_module.flavor)
+
+
+
+from my_module import greet, flavor
+
+greet("einstein")
+print("my favorite ice cream flavor is", flavor)
---
+++ Python/1-Fundamentals/week2 - For loops and functions/use_pckg.py
@@ -0,0 +1,3 @@
+from my_pkg import convert
+
+print(convert.ft_to_in(1))
